4240b8a53edbab946ada6b2329a7296c8219bb07
Hritvik Gupta <hritvik7654@gmail.com>
2025-06-01 18:35:46 -0400

    updated index.js

--- backend-python/main.py
+++ backend-python/main.py
@@ -56,7 +56,7 @@ import time
 import hashlib
 
 # Keep all existing imports
-
+from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core import Document, VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.ingestion import IngestionPipeline
