ddaa11db0cad2eccab5b51711f129aad83593f98
Hritvik Gupta <hritvik7654@gmail.com>
2025-06-05 13:26:48 -0400

    updated index.js

--- backend-python/main.py
+++ backend-python/main.py
@@ -10968,7 +10968,7 @@ def call_vertex_endpoint(prompt):
     
     data = {
         "instances": [{"prompt": prompt}],
-        "parameters": {"temperature": 0.0, "max_output_tokens": 2000}
+        "parameters": {"temperature": 0.0, "max_output_tokens": 3000}
     }
     
     # Make the request
